892c3e21f194eff8addd75408ff19409037d83be
BingoJYB <7303639+BingoJYB@users.noreply.github.com>
2018-10-10 18:01:38 +0200

    Add pagination

--- Tracker-Client/src/app/app.component.ts
+++ Tracker-Client/src/app/app.component.ts
@@ -1,81 +1,85 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  second = '00';
-  minute = '00';
-  hour = '00';
-  interval;
-  selectedDate;
+    second = '00';
+    minute = '00';
+    hour = '00';
+    interval;
+    selectedDate;
+    page: number = 1;
 
-  TASKS = [
-    {id: '1', start: '10/09/2018 07:00:00', end: '10/09/2018 08:00:00', description: 'Breakfast'},
-    {id: '2', start: '10/09/2018 11:00:00', end: '10/09/2018 12:00:00', description: 'Lunch'},
-    {id: '3', start: '10/08/2018 15:00:00', end: '10/08/2018 16:00:00', description: 'Rest'},
-    {id: '4', start: '10/07/2018 18:00:00', end: '10/07/2018 19:00:00', description: 'Dinner'},
-    {id: '5', start: '10/07/2018 21:00:00', end: '10/07/2018 23:00:00', description: 'Watch TV'}
-  ];
-  tasks = Object.assign([], this.TASKS);
+    TASKS = [
+        {id: '1', start: '10/09/2018 07:00:00', end: '10/09/2018 08:00:00', description: 'Breakfast'},
+        {id: '2', start: '10/09/2018 11:00:00', end: '10/09/2018 12:00:00', description: 'Lunch'},
+        {id: '3', start: '10/08/2018 15:00:00', end: '10/08/2018 16:00:00', description: 'Rest'},
+        {id: '4', start: '10/07/2018 18:00:00', end: '10/07/2018 19:00:00', description: 'Dinner'},
+        {id: '5', start: '10/07/2018 21:00:00', end: '10/07/2018 23:00:00', description: 'Watch TV'},
+        {id: '6', start: '10/07/2018 21:00:00', end: '10/07/2018 23:00:00', description: 'Watch TV'},
+        {id: '7', start: '10/07/2018 21:00:00', end: '10/07/2018 23:00:00', description: 'Watch TV'},
+        {id: '8', start: '10/07/2018 21:00:00', end: '10/07/2018 23:00:00', description: 'Watch TV'}
+    ];
+    tasks = Object.assign([], this.TASKS);
 
-  startTimer() {
-    this.pauseTimer();
-    this.interval = setInterval(() => {
-      let s = +this.second;
-      let m = +this.minute;
-      let h = +this.hour;
-      s++;
-      if (s > 59) {
-        s = 0;
-        m++;
-      }
-      if (m > 59) {
-        m = 0;
-        h++;
-      }
-      if (s < 10) {
-        this.second = '0' + String(s);
-      } else {
-        this.second = String(s);
-      }
-      if (m < 10) {
-        this.minute = '0' + String(m);
-      } else {
-        this.minute = String(m);
-      }
-      if (h < 10) {
-        this.hour = '0' + String(h);
-      } else {
-        this.hour = String(h);
-      }
-    }, 1000);
-  }
-
-  pauseTimer() {
-    clearInterval(this.interval);
-  }
-
-  resumeTimer() {
-    this.second = '00';
-    this.minute = '00';
-    this.hour = '00';
-    this.pauseTimer();
-  }
-
-  filterTask(selectedDate) {
-    if (selectedDate === undefined || selectedDate === '') {
-      this.tasks = this.TASKS;
-    } else {
-      this.tasks = this.TASKS.filter(task => {
-        const date = new Date(task.start.split(' ')[0]);
-        selectedDate = new Date(selectedDate);
-        return date.getFullYear() === selectedDate.getFullYear()
-          && date.getMonth() === selectedDate.getMonth()
-          && date.getDate() === selectedDate.getDate();
-      });
+    startTimer() {
+        this.pauseTimer();
+        this.interval = setInterval(() => {
+            let s = +this.second;
+            let m = +this.minute;
+            let h = +this.hour;
+            s++;
+            if (s > 59) {
+                s = 0;
+                m++;
+            }
+            if (m > 59) {
+                m = 0;
+                h++;
+            }
+            if (s < 10) {
+                this.second = '0' + String(s);
+            } else {
+                this.second = String(s);
+            }
+            if (m < 10) {
+                this.minute = '0' + String(m);
+            } else {
+                this.minute = String(m);
+            }
+            if (h < 10) {
+                this.hour = '0' + String(h);
+            } else {
+                this.hour = String(h);
+            }
+        }, 1000);
     }
-  }
+
+    pauseTimer() {
+        clearInterval(this.interval);
+    }
+
+    resumeTimer() {
+        this.second = '00';
+        this.minute = '00';
+        this.hour = '00';
+        this.pauseTimer();
+    }
+
+    filterTask(selectedDate) {
+        if (selectedDate === undefined || selectedDate === '') {
+            this.tasks = this.TASKS;
+        } else {
+            this.tasks = this.TASKS.filter(task => {
+                const date = new Date(task.start.split(' ')[0]);
+                selectedDate = new Date(selectedDate);
+                return date.getFullYear() === selectedDate.getFullYear()
+                    && date.getMonth() === selectedDate.getMonth()
+                    && date.getDate() === selectedDate.getDate();
+            });
+        }
+    }
 }
--- Tracker-Client/src/app/app.module.ts
+++ Tracker-Client/src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
 
 
-import { AppComponent } from './app.component';
-import { TaskComponent } from './task/task.component';
+import {AppComponent} from './app.component';
+import {TaskComponent} from './task/task.component';
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TaskComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TaskComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        NgxPaginationModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
